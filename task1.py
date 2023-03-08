c936e3bfe5292b7eb5a227a8065c735b617f2fa0
Yifan Wu <yifanwu@YifandeMacBook-Pro.local>
2023-03-07 20:01:37 -0500

    update task 1

--- task1.py
+++ task1.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
     sc = SparkContext()
     issued_lines = sc.textFile(sys.argv[1], 1)
     issued_lines = issued_lines.mapPartitions(lambda x: reader(x))
+    issued_lines = issued_lines.map(lambda x: (x[0], "%s, %s, %s, %s" % (x[3], x[16], x[2], x[1])))
 
     open_lines = sc.textFile(sys.argv[2], 1)
     open_lines = open_lines.mapPartitions(lambda x: reader(x))
-    open_SN = open_lines.map(lambda x: x[3]).collect()[1:]
+    open_lines = open_lines.map(lambda x: (x[0], ""))
 
-    paid_lines = issued_lines.filter(lambda x: x[0] not in open_SN) \
-        .map(lambda x: "%s\t%s, %s, %s, %s" % (x[0], x[3], x[16], x[2], x[1]))
+    paid_lines = issued_lines.leftOuterJoin(open_lines).filter(lambda x: x[1][1]==None).map(lambda x: "%s\t %s" % (x[0], x[1][0]))
 
     paid_lines.saveAsTextFile("task1.out")
 
