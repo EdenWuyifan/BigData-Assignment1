e394b9bf81194154d1d50b01c84341a0d65f1965
Yifan Wu <yifanwu@YifandeMacBook-Pro.local>
2023-03-07 20:39:32 -0500

    task 4 debug

--- task4-sql.py
+++ task4-sql.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     issued.createOrReplaceTempView("issued")
 
     result = spark.sql("SELECT registration_state, COUNT(registration_state) AS count FROM (SELECT CASE WHEN issued.registration_state LIKE 'NY' THEN 'NY' ELSE 'Other' END AS registration_state FROM issued) GROUP BY registration_state")
-    result.select(format_string('%s\t %d', result.registration_state, result.count)).write.save("sql.out", format="text")
+    result.select(format_string('%s\t %d', result.registration_state, result.count)).write.save("task4-sql.out", format="text")
 
     sc.stop()
